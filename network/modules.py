eaa2c4f615735f3e7d8caeb2b59325357271a21b
annaproxy <annaproxy@protonmail.com>
2020-01-12 00:12:51 +0100

    basic mnist train- loss goes down so at least something works

--- network/modules.py
+++ network/modules.py
@@ -11,36 +11,31 @@ class ConvEncoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # Initialize the ConvNet
         # Apparently padding=1 was necessary to get the same dimensions as listed in the paper.
         # There should be a way to do this automatically, which is what tensorflow probably does 
         # Although in the paper "zero padding" is used, which is kinda ambiguous in itself 
         self.convnet = nn.Sequential(
             nn.Conv2d(1, 32,  kernel_size = 3, stride = 2, padding=1),
-            nn.Sigmoid()
-            
-        )
-        self.mid = nn.Sequential(
+            nn.Sigmoid(),
             nn.Conv2d(32, 32, kernel_size = 3, stride = 2, padding=1),
             nn.Sigmoid(),
             nn.Conv2d(32, 32, kernel_size = 3, stride = 2, padding=1),
-            nn.Sigmoid()
-        )
-
-        self.final = nn.Sequential(nn.Conv2d(32, 10, kernel_size = 3, stride = 2, padding=1),
+            nn.Sigmoid(),
+            nn.Conv2d(32, 10, kernel_size = 3, stride = 2, padding=1),
             nn.Sigmoid())
 
     def forward(self, input):
         """
-        Perform forward pass of encoder for convolutional network.
+        Perform forward pass of CNN encoder
+        Args:
+            Input:
+                input : MNIST sized image of shape (batch_size, 1, 28, 28)
+
+            Output:
+                out : Encoded data of shape  (batch_size * 10 * 2 * 2)
         """
         # Perform full pass through network
         out = self.convnet(input)
-        print("Shape after first layer\t", out.shape)
-        out = self.mid(out)
-        print("Shape after middle layers\t", out.shape)
-        out = self.final(out)
-        print("Final shape\t", out.shape)
         return out
 
 class ConvDecoder(nn.Module):
@@ -51,13 +46,19 @@ class ConvDecoder(nn.Module):
         self.de1 = nn.ConvTranspose2d(10,32,kernel_size=3, stride=2,padding=1)
         self.de2 = nn.ConvTranspose2d(32,32,kernel_size=3, stride=2,padding=1)
         self.de3 = nn.ConvTranspose2d(32,32,kernel_size=3, stride=2,padding=1)
-        self.de4 = nn.ConvTranspose2d(32,1,kernel_size=3, stride=2,padding=1)
+        self.de4 = nn.ConvTranspose2d(32,1, kernel_size=3, stride=2,padding=1)
         self.sigmoid = nn.Sigmoid()
 
 
     def forward(self, input):
         """
-        Perform forward pass of decoder.
+        Perform forward pass of the CNN decoder
+        Args:
+            Input: 
+                input : Encoded data of shape (batch_size * 10 * 2 * 2)
+        
+            Output:
+                out : Decoded data of shape (batch_size * 1 * 28 * 28)
         """
         # Output_size is necessary during convolution
         b = len(input)
--- train.py
+++ train.py
@@ -2,23 +2,58 @@ import numpy as np
 import torch 
 import torch.nn as nn
 from model import PrototypeModel
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.nn.functional import one_hot
 
-images = torch.randn(100, 1, 28, 28)
-labels = torch.randint(9, (100,))
-m = PrototypeModel(14, 40, 10)
-enc, dec, (p, c) = m.forward(images)
-print(enc.shape, dec.shape, p.shape, c.shape)
+train_data = MNIST('./data', train=True, download=True, transform=transforms.Compose([
+                                              transforms.ToTensor(),
+                                              transforms.Normalize((0.5,), (0.5,))
+                                          ]))
+test_data = MNIST('./data', train=False, download=True,transform=transforms.Compose([
+                                              transforms.ToTensor(),
+                                              transforms.Normalize((0.5,), (0.5,))
+                                          ]))
 
+# training parameters
+learning_rate = 0.002      # Adam
+training_epochs = 10       # epochs
+batch_size = 250           # the size of a minibatch
+#test_display_step = 100   # how many epochs we do evaluate on the test set once
+#save_step = 50            # how frequently do we save the model to disk
 
-# Calculate loss: Crossentropy + Reconstruction + R1 + R2 
-# Crossentropy h(f(x)) and y
-ce = nn.CrossEntropyLoss()
-# reconstruction error g(f(x)) and x
-re = torch.mean(torch.norm(dec - images) ** 2)
-# regularization r1: Be close to at least one training example (get min distance to each datapoint=dimension 0)
-r1 = torch.mean(torch.min(p, axis=0)[0])
-# regularization r2: Be close to at least one prototype (get min distance to each prototype=dimension 1)
-r2 = torch.mean(torch.min(p, axis=1)[0])
+proto = PrototypeModel(14, 40, 10)
+optim = torch.optim.Adam(proto.parameters(), lr=learning_rate)
 
-loss = 20*ce(c, labels) + re + r1 + r2
-print(loss)
+dataloader = DataLoader(train_data, batch_size=batch_size)
+
+it = 0
+for epoch in range(training_epochs):
+    epoch_loss = 0.0
+    for i, (images, labels) in enumerate(dataloader):
+        # TODO: Warp image data first.
+        it += 1
+        labels = one_hot(labels)
+        enc, dec, (p, c) = proto.forward(images)
+
+        # Calculate loss: Crossentropy + Reconstruction + R1 + R2 
+        # Crossentropy h(f(x)) and y
+        ce = nn.CrossEntropyLoss()
+        # reconstruction error g(f(x)) and x
+        re = torch.mean(torch.norm(dec - images) ** 2)
+        # regularization r1: Be close to at least one training example (get min distance to each datapoint=dimension 0)
+        r1 = torch.mean(torch.min(p, axis=0)[0])
+        # regularization r2: Be close to at least one prototype (get min distance to each prototype=dimension 1)
+        r2 = torch.mean(torch.min(p, axis=1)[0])
+
+        # Paper does 20 * ce and lambda_n = 1 for each regularization term
+        loss = 20*ce(c, torch.argmax(labels, dim=1)) + re + r1 + r2
+        print(loss)
+        epoch_loss += loss.item()
+
+        loss.backward()
+        optim.step()
+        optim.zero_grad()
+        
+    print(epoch, epoch_loss / it)

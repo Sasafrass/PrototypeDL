fixed reconstruction normalization

train.py
@@ -54,7 +54,8 @@ def train_MNIST(learning_rate=0.002, training_epochs=650, batch_size=250, sigma=
             # Crossentropy h(f(x)) and y
             ce = nn.CrossEntropyLoss()
             # reconstruction error g(f(x)) and x
-            re = torch.mean(torch.norm(dec - images) ** 2)
+            subtr = (dec - images).view(-1, 28*28)
+            re = torch.mean(torch.norm(subtr, dim=1))
             
             # Paper does 20 * ce and lambda_n = 1 for each regularization term
             # Calculate loss and get accuracy etc.
3f31206a927e7c1b05742be2e12009ed58824032
hinriksnaer <hinriksnaer@gmail.com>
2020-01-15 14:38:32 +0100

    refactoring

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import torch 
 import torch.nn as nn
 from torchvision.datasets import MNIST
@@ -7,13 +8,79 @@ import torchvision.transforms as transforms
 from torch.nn.functional import one_hot
 from torchvision.utils import save_image
 from preprocessing import batch_elastic_transform
-
 from model import PrototypeModel
 
 
 # Global parameters for device and reproducibility
 torch.manual_seed(7)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Globals
+learning_rate = 0.0001
+training_epoch = 1500
+batch_size = 250
+
+sigma = 4
+alpha = 20
+n_prototypes = 15
+latent_size = 40
+n_classes = 10
+
+lambda_class = 20
+lambda_ae = 1
+lambda_1 = 1              # 1 and 2 here corresponds to the notation we used in the paper
+lambda_2 = 1
+
+model_path = 'models/'
+prototype_path = 'images/prototypes/'
+decoding_path = 'images/decoding/'
+
+def run_epoch(model, dataloader, optimizer, iteration, epoch_loss, epoch_accuracy):
+    for i, (images, labels) in enumerate(dataloader):
+
+            # Up the iteration by 1
+            iteration += 1
+
+            # Transform images, then port to GPU
+            images = batch_elastic_transform(images, sigma, alpha, 28, 28)
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Warp image data first.
+            oh_labels = one_hot(labels)
+            _, decoding, (r1, r2, c) = model.forward(images)
+
+            # Calculate loss: Crossentropy + Reconstruction + R1 + R2 
+            # Crossentropy h(f(x)) and y
+            ce = nn.CrossEntropyLoss()
+            # reconstruction error g(f(x)) and x
+            subtr = (decoding - images).view(-1, 28*28)
+            re = torch.mean(torch.norm(subtr, dim=1))
+            
+            # Paper does 20 * ce and lambda_n = 1 for each regularization term
+            # Calculate loss and get accuracy etc.
+            loss = lambda_class * ce(c, torch.argmax(oh_labels, dim=1)) + lambda_ae * r1 + lambda_1 * r2 + lambda_2 * re
+            #print( r1, r2)
+            epoch_loss += loss.item()
+            preds = torch.argmax(c,dim=1)
+            corr = torch.sum(torch.eq(preds,labels))
+            size = labels.shape[0]
+            epoch_accuracy += corr.item()/size
+
+            # Do backward pass and ADAM steps
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+    return iteration, epoch_loss, epoch_accuracy, decoding
+
+def save_images(prototype_path, decoding_path, prototypes, decoding, epoch):
+    if not os.path.exists(prototype_path):
+            os.makedirs(prototype_path)
+    if not os.path.exists(decoding_path):
+        os.makedirs(decoding_path)
+    save_image(prototypes, prototype_path+'prot{}.png'.format(epoch), nrow=5, normalize=True)
+    save_image(decoding, decoding_path+'dec{}.png'.format(epoch), nrow=5, normalize=True)
+
 
 def train_MNIST(learning_rate=0.0001, training_epochs=1500, batch_size=250, sigma=4, alpha=20):
     # Load data
@@ -35,53 +102,21 @@ def train_MNIST(learning_rate=0.0001, training_epochs=1500, batch_size=250, sigm
         epoch_loss = 0.0
         epoch_acc = 0.0
         it = 0
-        for i, (images, labels) in enumerate(dataloader):
-
-            # Up the iteration by 1
-            it += 1
-
-            # Transform images, then port to GPU
-            images = batch_elastic_transform(images, sigma, alpha, 28, 28)
-            images = images.to(device)
-            labels = labels.to(device)
-
-            # Warp image data first.
-            oh_labels = one_hot(labels)
-            _, dec, (r1, r2, c) = proto.forward(images)
-
-            # Calculate loss: Crossentropy + Reconstruction + R1 + R2 
-            # Crossentropy h(f(x)) and y
-            ce = nn.CrossEntropyLoss()
-            # reconstruction error g(f(x)) and x
-            subtr = (dec - images).view(-1, 28*28)
-            re = torch.mean(torch.norm(subtr, dim=1))
-            
-            # Paper does 20 * ce and lambda_n = 1 for each regularization term
-            # Calculate loss and get accuracy etc.
-            loss = 20*ce(c, torch.argmax(oh_labels, dim=1)) + r1 + r2 + re
-            #print( r1, r2)
-            epoch_loss += loss.item()
-            preds = torch.argmax(c,dim=1)
-            corr = torch.sum(torch.eq(preds,labels))
-            size = labels.shape[0]
-            epoch_acc += corr.item()/size
-
-            # Do backward pass and ADAM steps
-            loss.backward()
-            optim.step()
-            optim.zero_grad()
+        
+        it, epoch_loss, epoch_acc, dec = run_epoch(proto, dataloader, optim, it, epoch_loss, epoch_acc)
 
         # Get prototypes and decode them to display
         prototypes = proto.prototype.get_prototypes()
-        prototypes = prototypes.view(-1,10,2,2)
+        prototypes = prototypes.view(-1, 10, 2, 2)
         imgs = proto.decoder(prototypes)
 
         # Save images
-        save_image(imgs, 'images/prot{}.png'.format(epoch), nrow=5, normalize=True)
-        save_image(dec, 'images/dec{}.png'.format(epoch), nrow=5, normalize=True)
+        save_images(prototype_path, decoding_path, imgs, dec, epoch)
 
         # Save model
-        torch.save(proto, "models/proto.pth")
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
+        torch.save(proto, model_path+"proto.pth")
 
         # Print statement to check on progress
         print("Epoch: ", epoch, "Loss: ", epoch_loss / it, "Acc: ", epoch_acc/it)

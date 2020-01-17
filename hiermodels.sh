105a423b17d7db52daaf63b4f944751bbf4d1d5c
Albert Harkema <lgpu0001@test3.irc.sara.nl.irc.sara.nl>
2020-01-17 11:35:29 +0100

    changes in bash file

--- hiermodels.sh
+++ hiermodels.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -t -02:30:00
-#SBATCH --partition=gpu_shared
+#SBATCH -t 02:30:00
+#SBATCH --partition=gpu_shared_course
 #SBATCH -n 2
 module load pre2019
 module load Miniconda3/4.3.27
 source activate prototype2
 
-python3 train.py
+python train.py
